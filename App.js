4b65f0e2ee19ddd47479ca3a3aa14088984eb9b2
Naren-Jegan <naren.jegan26@gmail.com>
2021-03-30 19:35:21 +1100

    Basic App with Map and Marker

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <MapView
+      style={StyleSheet.absoluteFillObject}
+      provider={MapView.PROVIDER_GOOGLE}
+      initialRegion={{
+        latitude: -37.8186,
+        longitude: 144.9637,
+        latitudeDelta: 0.10,
+        longitudeDelta: 0.10
+      }}
+      >
+        <Marker
+        coordinate={{latitude: -37.8176783959, longitude: 144.951446194}}
+        title={'Southern Cross Station'}
+        description={'Spencer St, Docklands VIC 3008'}
+        />
+      </MapView>
       <StatusBar style="auto" />
     </View>
   );
